7d572a1d28671369a0ad3be7de3cf262e99d4b05
Stackbit Code Editor <projects@stackbit.com>
2021-06-01 04:07:07 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -15,7 +15,7 @@ import FormField from './FormField';
 import FormSection from './FormSection';
 import GridItem from './GridItem';
 import GridSection from './GridSection';
-import CustomGridItem from './CustomGridItem';
+import CustomItems from './CustomItems';
 import CustomGrid from './CustomGrid';
 import Header from './Header';
 import HeaderMenu from './HeaderMenu';
