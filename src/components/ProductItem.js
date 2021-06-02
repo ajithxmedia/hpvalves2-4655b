39590dfb2e2d59ad6bc0d6f586ad436540999203
Stackbit Code Editor <projects@stackbit.com>
2021-06-02 06:28:19 +0000

    ProductItem.js: changed

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import {classNames, withPrefix, markdownify} from '../utils';
 import SectionActions from './SectionActions';
 
-export default class GridItem extends React.Component {
+export default class ProductItem extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         let item = _.get(this.props, 'item', null);
