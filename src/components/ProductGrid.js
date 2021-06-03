be95095729138d88f17ed56a11011b79db9a4c2e
Stackbit Code Editor <projects@stackbit.com>
2021-06-03 04:12:27 +0000

    ProductGrid.js: changed

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -43,7 +43,8 @@ export default class ProductGrid extends React.Component {
                         {/*Left Container  */}
                         <div className={classNames('cell-12', 'cell-md-3','cell-lg-3','cell-sm-12','my-1')}>
                             <div class="tab-sections">
-                                <ul class="tab_container"></ul>
+                                <ul class="tab_container">
+                                </ul>
                             </div>
                             {_.map(_.get(section, 'tabs_items', null), (item, item_idx) => (
                                  <div>{_.get(item, 'label', null)}</div>           
